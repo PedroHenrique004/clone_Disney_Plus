61e4f23300c77c55d0c6c5f6b81a4993219bb3fa
Pedro Santos <pedrohenriqueph004@gmail.com>
2023-12-10 16:27:59 -0300

    Criando o header e ajustando para mobile

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+    const headerElements = document.querySelector('[data-tab-header]');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            headerElements.classList.remove('header--is-hidden');
+        }
+        else {
+            headerElements.classList.add('header--is-hidden');
+        }
+    })
+
     
-    
-    for (let i =0; i< buttons.length; i++){
+    for (let i =0; i< buttons.lengtH; i++){
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
@@ -12,6 +28,18 @@ document.addEventListener('DOMContentLoaded', function(){
             botao.target.classList.add('shows__tabs__button--is-active')
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta)
+    }
+
+    function abreOuFechaResposta(elemento){
+       const classe ='faq__questions__item--is-open';
+       const elementoPai =  elemento.target.parentNode;
+
+       elementoPai.classList.toggle(classe);
+    }
+    
 
     function removeBotaoAtivo (){
         const buttons = document.querySelectorAll('[data-tab-button]');
